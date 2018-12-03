b8bcd7d2fa9a765d6ca96d49ab67ec4f2ca09f4a
zorba <adriano.vc.campinho@gmail.com>
2018-12-03 20:40:03 +0000

    script

--- tp3/omdbApi.py
+++ tp3/omdbApi.py
@@ -1,14 +1,33 @@
 
 import omdb
 import csv
+import pandas
+import json
 
-omdb.set_default('apikey', 'd6f82b99') #MUDAR PARA CADA APIKEY
 
-with open('./dif.csv', 'rb') as csvfile:
+# SO MUDAR ISTO -----------------------------------------------------------
+pathFile = './ml-latest-small/imdblinks0.csv'
+
+apiKey = 'd6f82b99'
+# ADRIANO = d6f82b99
+# LUIS = ?
+# EZEQUIEL = ?
+
+# SO MUDAR ISTO -----------------------------------------------------------
+
+omdb.set_default('apikey', apiKey) 
+d = {}
+
+with open(pathFile, 'rb') as csvfile:
     for row in csvfile:
-        #res = omdb.get(imdbid=row, fullplot=False, tomatoes=False)
-        #print res
-        print(row)
 
-    
+        ref = row.decode("utf-8")[0:-1]
+        res = omdb.get(imdbid=ref, fullplot=False, tomatoes=False)
+        d[ref] = res
+
+
+final = pandas.read_json(json.dumps(d, ensure_ascii=False))
+print(final)
+
+
 
