df0152d09a7aabe57f702eb745b986a792033d5a
zorba <adriano.vc.campinho@gmail.com>
2018-12-08 20:37:03 +0000

    Almost all data finally

--- tp3/omdbApi.py
+++ tp3/omdbApi.py
@@ -5,7 +5,7 @@ import json
 
 
 # SO MUDAR ISTO -----------------------------------------------------------
-numero_link = 0
+numero_link = 3
 pathFile = './ml-latest-small/' + 'link' + str(numero_link) + '.csv'
 
 ADRIANO = 'd6f82b99'
@@ -15,8 +15,10 @@ LUIS2 = 'fb25258'
 LUIS3 = 'ade8eee5'
 EZEQUIEL = '14459687'
 AUX = '34b11df4' 
+AUX2 = '1e6f36b'
+AUX3 = 'e85a658a'
 
-apiKey = AUX
+apiKey = AUX3
 
 # SO MUDAR ISTO -----------------------------------------------------------
 
@@ -32,10 +34,9 @@ with open(pathFile, 'rb') as csvfile:
         d[ref] = res
 
         #if(i==2):
-        #   final = pandas.read_json(json.dumps(d, ensure_ascii=False))
-        #   final.to_csv('res' + str(numero_link)+'.csv', header = True, index = False)
+         #  final = pandas.read_json(json.dumps(d, ensure_ascii=False))
+         #  final.to_csv('resAuxiliar' + str(numero_link)+'.csv', header = True, index = True)
         #   exit()
 
 final = pandas.read_json(json.dumps(d, ensure_ascii=False))
-final.to_csv('res' + str(numero_link)+'.csv', header = True, index = False, sep = '£' )
-
+final.to_csv('resAux' + str(numero_link)+'.csv', header = True, index = True, sep = ';')
