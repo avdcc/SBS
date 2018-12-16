dcf6e430babafe1eb94b859d10842f21d11cb437
zorba <adriano.vc.campinho@gmail.com>
2018-12-16 20:46:38 +0000

    stuff

--- tp3/teste.py
+++ tp3/teste.py
@@ -9,7 +9,7 @@ import re
 movies = pd.read_csv('filmes.csv', sep=';', encoding='utf-8')
 
 # ------------------------------------------------------------------------
-# Gera sugestoes com base em features de um filme
+# Content Based
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
@@ -19,7 +19,7 @@ titles = movies['title']
 indices = pd.Series(movies.index, index=movies['title'])
 
 # Function that get movie recommendations based on the cosine similarity score of movie genres
-def recommendations(title,feature):
+def recommendationsMatrix(title, feature):
 
     # Break up the big genre string into a string array and Convert genres to string value
     movies[feature] = movies[feature].str.split(', ')
@@ -29,18 +29,54 @@ def recommendations(title,feature):
 
     from sklearn.metrics.pairwise import linear_kernel
     cosine_sim = linear_kernel(matrix, matrix)
+    return(cosine_sim)
 
+def completeRecommendations(title, features, filters, nRes):
+
+    mx = np.empty([len(titles),len(titles)])
+    for f in features:
+        mx += recommendationsMatrix(title, f)
+        
     idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = list(enumerate(mx[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:21]
+    
+    for f in filters:
+        sim_scores = list(filter(lambda x: movies[f][x[0]] == movies[f][idx], sim_scores))
+  
+    sim_scores = list(filter(lambda x: x[0] != idx, sim_scores))
+    sim_scores = sim_scores[0:nRes]
+
     movie_indices = [i[0] for i in sim_scores]
-    print (type(titles.iloc[movie_indices]))
-    return titles.iloc[movie_indices]
 
+    return titles.iloc[movie_indices].tolist()
+
+
+# ------------------------------------------
 # - Print Resultados
+print(
+completeRecommendations('Dial M for Murder', ['genre','actors'],['production', 'year'], 100)
+)
+# "actors";"awards";"country";"director";"genre";"imdb_rating";"imdb_votes";"language";"metascore";"plot";"poster";"production";"ratings";"title";"writer";"year";"dvdYear";"releasedMonth";"releasedYear";"duration"
+# "country";"director";"genre";"imdb_rating";"imdb_votes";"language";"metascore";"plot";"poster";"production";"ratings";"title";"writer";"year";"dvdYear";"releasedMonth";"releasedYear";"duration"
 
-print(recommendations('Toy Story 2', 'genre').head(20))
-print(recommendations('Toy Story 2', 'actors').head(20))
 
 # ------------------------------------------------------------------------
+# Collaborative Filtering Recommendation
+
+# ratings = pd.read_csv('ratings.csv', sep=';', encoding='utf-8')
+
+# # Fill NaN values in user_id and movie_id column with 0
+# ratings['user_id'] = ratings['user_id'].fillna(0)
+# ratings['movie_id'] = ratings['movie_id'].fillna(0)
+
+# # Replace NaN values in rating column with average of all values
+# ratings['rating'] = ratings['rating'].fillna(ratings['rating'].mean())
+
+
+
+# from sklearn.metrics.pairwise import pairwise_distances
+# # User Similarity Matrix
+# user_correlation = 1 - pairwise_distances(ratings, metric='correlation')
+# user_correlation[np.isnan(user_correlation)] = 0
+# print(user_correlation[:4, :4])
