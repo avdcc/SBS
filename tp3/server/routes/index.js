7e706f97c631b8db015ebb482bd38b36d96d333a
iamtruth <Ezequiel-Moreira>
2018-12-19 17:43:37 +0000

    made the interface prettier

--- tp3/server/routes/index.js
+++ tp3/server/routes/index.js
@@ -3,21 +3,23 @@
 var express = require('express');
 var router = express.Router();
 
+var websiteTitle = 'title'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render(nome_do_pug_a_carregar,argumentos_a_passar_ao_pug)
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: websiteTitle });
 });
 
 
 
 //content-based filtering
 
-router.get('/contFilt',()=>{
-  res.render('content',{})
+router.get('/contFilt',(req,res)=>{
+  res.render('content',{ title: websiteTitle })
 })
 
-router.post('/contFilt',()=>{
+router.post('/contFilt',(req,res)=>{
   //here we will handle the actual data passed from the user
 })
 
@@ -25,11 +27,11 @@ router.post('/contFilt',()=>{
 
 //collaborative filtering
 
-router.get('/collFilt',()=>{
-  res.render('collaborative',{})
+router.get('/collFilt',(req,res)=>{
+  res.render('collaborative',{ title: websiteTitle })
 })
 
-router.post('/collFilt',()=>{
+router.post('/collFilt',(req,res)=>{
   //here we will handle the actual data passed from the user
 })
 
