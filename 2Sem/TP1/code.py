44c723d136e5f30383a988163e05c33d6190e0fe
Adriano <adriano.vc.campinho@gmail.com>
2019-03-19 18:13:27 +0000

    lot changed

--- 2Sem/TP1/code.py
+++ 2Sem/TP1/code.py
@@ -1,5 +1,4 @@
 
-#imports
 import gym
 
 import tensorflow as tf
@@ -8,52 +7,107 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 import os
 import random
 import imageio
 from skimage.transform import resize
 
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
 import matplotlib.pyplot as plt
 import cv2
 
+# ---------------------------------------------------------------------------------
+# GLOBAIS
+
+ENV_NAME = "LunarLander-v2" # "Breakout-v0"
+
+GAMMA = 0.95
+LEARNING_RATE = 0.001
+
+MEMORY_SIZE = 1000000
+BATCH_SIZE = 20
+
+NUMBER_OF_EPISODES = 25
+
+EXPLORATION_MAX = 1.0
+EXPLORATION_MIN = 0.01
+EXPLORATION_DECAY = 0.995
+
+# FLAGS = tf.app.flags.FLAGS
+
+# tf.app.flags.DEFINE_string('train_dir', 'tf_train_breakout',"""Directory where to write event logs and checkpoint. """)
+# tf.app.flags.DEFINE_string('restore_file_path','./coiso.h5',"""Path of the restore file """)
+# tf.app.flags.DEFINE_integer('num_episode', 100000,"""number of epochs of the optimization loop.""")
+# # tf.app.flags.DEFINE_integer('observe_step_num', 5000,
+# tf.app.flags.DEFINE_integer('observe_step_num', 50000,"""Timesteps to observe before training.""")
+# # tf.app.flags.DEFINE_integer('epsilon_step_num', 50000,
+# tf.app.flags.DEFINE_integer('epsilon_step_num', 1000000,"""frames over which to anneal epsilon.""")
+# tf.app.flags.DEFINE_integer('refresh_target_model_num', 10000,  # update the target Q model every refresh_target_model_num
+#               """frames over which to anneal epsilon.""")
+# tf.app.flags.DEFINE_integer('replay_memory', 400000,  # takes up to 20 GB to store this amount of history data
+#               """number of previous transitions to remember.""")
+# tf.app.flags.DEFINE_integer('no_op_steps', 30,"""Number of the steps that runs before script begin.""")
+# tf.app.flags.DEFINE_float('regularizer_scale', 0.01,"""L1 regularizer scale.""")
+# tf.app.flags.DEFINE_integer('batch_size', 32,"""Size of minibatch to train.""")
+# tf.app.flags.DEFINE_float('learning_rate', 0.00025,"""Number of batches to run.""")
+# tf.app.flags.DEFINE_float('init_epsilon', 1.0,"""starting value of epsilon.""")
+# tf.app.flags.DEFINE_float('final_epsilon', 0.1,"""final value of epsilon.""")
+# tf.app.flags.DEFINE_float('gamma', 0.99,"""decay rate of past observations.""")
+# tf.app.flags.DEFINE_boolean('resume', False,"""Whether to resume from previous checkpoint.""")
+# tf.app.flags.DEFINE_boolean('render', False,"""Whether to display the game.""")
+
+# ATARI_SHAPE = (84, 84, 4)  # input image size to model
+# ACTION_SIZE = 3
 
 # ---------------------------------------------------------------------------------
+# MODELO
 
-'''
-Pré-processamento(declaração de variáveis necessárias ao modelo)
-'''
+class DQNSolver:
 
-#criar ambiente gym do jogo
-env = gym.make('Breakout-v0')
-env.reset()
+  def __init__(self, observation_space, action_space):
+    self.exploration_rate = EXPLORATION_MAX
 
-#número de camadas do modelo que criaremos
-#TODO: determinar números que melhorem o resultado
-input_layers = 64
-output_layers = 10
+    self.action_space = action_space
+    self.memory = deque(maxlen=MEMORY_SIZE)
 
-#tamanho de passo usado para o otimizador na compilação
-#TODO: determinar o valor otimo para esta variável
-step_size = 0.001
+    self.model = Sequential()
+    self.model.add(Dense(24, input_shape=(observation_space,), activation="relu"))
+    self.model.add(Dense(24, activation="relu"))
+    self.model.add(Dense(self.action_space, activation="linear"))
+    self.model.compile(loss="mse", optimizer=Adam(lr=LEARNING_RATE))
 
-#número de epocas que devem ser usadas
-ephocs = 10
+  def remember(self, state, action, reward, next_state, done):
+    self.memory.append((state, action, reward, next_state, done))
 
-#nome do ficheiro onde será guardado o modelo
-#e depois carregado
-save_filename = "test.h5"
+  def act(self, state):
+    if np.random.rand() < self.exploration_rate:
+      return random.randrange(self.action_space)
+    q_values = self.model.predict(state)
+    return np.argmax(q_values[0])
 
+  def experience_replay(self):
+    if len(self.memory) < BATCH_SIZE:
+      return
+    batch = random.sample(self.memory, BATCH_SIZE)
+    for state, action, reward, state_next, terminal in batch:
+      q_update = reward
+      if not terminal:
+        q_update = (reward + GAMMA * np.amax(self.model.predict(state_next)[0]))
+      q_values = self.model.predict(state)
+      q_values[0][action] = q_update
+      self.model.fit(state, q_values, verbose=0)
+    self.exploration_rate *= EXPLORATION_DECAY
+    self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
 
+# ---------------------------------------------------------------------------------
+# AUXILIARES
 
-'''
-Funções auxiliares
-'''
-
-
-# -------------------------------------------------
-
-#Processa a imagem removendo informacao desnecesaria
+# -----
+# Processa a imagem removendo informacao desnecessaria (no breackout)
 def processImage(observation):
     observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
     observation = observation[17:101,4:80]
@@ -74,19 +128,15 @@ def processImage(observation):
 # brain.setInitState(observation0)
 # brain.currentState = np.squeeze(brain.currentState)
 
-# ----------------------------------------------------
-
-
-
-#guardar o modelo model num certo ficheiro de nome filename
-#na subpasta models
+# -----
+# Guardar o modelo model num certo ficheiro de nome filename na subpasta models
 def save_model(model,filename):
   #ficheiro onde iremos guardar: models/filename
   file_saved_to = "models/" + filename
   #guardamos o modelo
   model.save(file_saved_to)
 
-#carrega o modelo do ficheiro filename da pasta models
+# Carrega o modelo do ficheiro filename da pasta models
 def load_model(filename):
   model = None
   #testar se o ficheiro existe
@@ -123,24 +173,15 @@ def build_model(input_size,output_size,input_layers,output_layers,step_size):
   #iniciar modelo sequencial
   model = keras.Sequential()
 
-  #adicionar camada de input
-  #nota: o abaixo assume que vamos usar um dense layer
-  #é possivel usar outra, e nesse caso temos de rever isto
-  #também assume relu como ativação, rever depois
+  #adicionar camada de input nota: o abaixo assume que vamos usar um dense layer é possivel usar outra, e nesse caso temos de rever isto também assume relu como ativação, rever depois
   input_layer = keras.layers.Dense(input_layers,input_dim=input_size,activation='relu')
   model.add(input_layer)
 
-  #adicionar camada intermédia
-  #nota: o abaixo assume que vamos usar um dense layer
-  #, mas em principio será assim para a hidden, rever depois
-  #também assume relu como ativação, rever depois
+  #adicionar camada intermédia nota: o abaixo assume que vamos usar um dense layer, mas em principio será assim para a hidden, rever depois também assume relu como ativação, rever depois
   hidden_layer = keras.layers.Dense(input_layers/2,activation='relu')
   model.add(hidden_layer)
 
-  #adicionar camada de output
-  #nota: o abaixo assume que vamos usar um dense layer
-  #, mas em principio será assim para a output, rever depois
-  #também assume softmax como ativação, rever depois
+  #adicionar camada de output nota: o abaixo assume que vamos usar um dense layer, mas em principio será assim para a output, rever depois também assume softmax como ativação, rever depois
   output_layer = keras.layers.Dense(output_layers,input_dim=output_size,activation='softmax')
   model.add()
 
@@ -228,9 +269,113 @@ Avaliação do modelo(calculo de erro)
 #nesta parte provavelmente apenas teremos
 #de colocar uma mensagem para ver qual a accuracy do modelo
 
+
+
+
+
+
+
+
+
+
+
+
+
+def runner():
+  env = gym.make(ENV_NAME)
+  observation_space = env.observation_space.shape[0]
+  action_space = env.action_space.n
+  dqn_solver = DQNSolver(observation_space, action_space)
+  run = 0
+  while True:
+    run += 1
+    state = env.reset()
+    state = np.reshape(state, [1, observation_space])
+    step = 0
+    while True:
+      step += 1
+      env.render()
+      action = dqn_solver.act(state)
+      state_next, reward, terminal, info = env.step(action)
+      reward = reward if not terminal else -reward
+      state_next = np.reshape(state_next, [1, observation_space])
+      dqn_solver.remember(state, action, reward, state_next, terminal)
+      state = state_next
+      if terminal:
+        print("Run: " + str(run) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(step))
+        
+        # score_logger.add_score(step, run)
+        break
+      dqn_solver.experience_replay()
+
+
+if __name__ == "__main__":
+  runner()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''
-Fazer previsões(por o modelo a jogar no ambiente)
-'''
+
+# ---------------------------------------------------------------------------------
+# Pre-processamento(declaracao de variaveis necessarias ao modelo)
+
+#criar ambiente gym do jogo
+env = gym.make(ENV_NAME)
+env.reset()
+
+#número de camadas do modelo que criaremos
+#TODO: determinar números que melhorem o resultado
+input_layers = 64
+output_layers = 10
+
+#tamanho de passo usado para o otimizador na compilação
+#TODO: determinar o valor otimo para esta variável
+step_size = 0.001
+
+#número de epocas que devem ser usadas
+ephocs = 10
+
+#nome do ficheiro onde será guardado o modelo
+#e depois carregado
+save_filename = "test.h5"
 
 
 
@@ -240,7 +385,7 @@ Fazer previsões(por o modelo a jogar no ambiente)
 
 
 #tem 20 episodios
-for i_episode in range(20):
+for i_episode in range(NUMBER_OF_EPISODES):
   done = False
   steps = 0
   #o reset retorna uma observação inicial do ambiente
@@ -268,9 +413,6 @@ for i_episode in range(20):
       print("debug info: {}".format(info))
       print("Episode finished after {} timesteps".format(steps+1))
 
-'''
-DEBUG
-'''
 
 #espaço de acções
 #é um array que
@@ -282,3 +424,5 @@ print(env.action_space)
 #é um array de dimensão (210, 160, 3)
 #que corresponde ao nº de pixeis do jogo
 print(env.observation_space)
+
+'''
