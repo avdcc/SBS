4dc77cda47e36b79ee7af3c95f45b701ab9c8199
ibanezg <ibanezg@ibanezg-System-Product-Name.home>
2019-05-25 19:32:55 +0100

    filter and sort data before a date

--- 2Sem/TP2/create_graph.py
+++ 2Sem/TP2/create_graph.py
@@ -1,10 +1,25 @@
 #city_name,description,cause_of_incident,from_road,to_road,affected_roads,incident_category_desc,magnitude_of_delay_desc,length_in_meters,delay_in_seconds,incident_date
 import sys
 import functools as f
+#import netgraph
+#import matplotlib.pyplot as plt; plt.ion()
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx import Graph
+import re
+import unidecode
+from datetime import datetime, timedelta
+from numpy import round
+
+#####################
+#     DEFINES       #
+#####################
+OUTPUT = ''
 
 def readFile(name_file):
     with open(name_file,'r') as fd:
-        return list(map( lambda x: x.split(','),fd.read().strip("'").strip('"').split('\n')))
+        #return list(map( lambda x: x.split(','),fd.read().strip("'").strip("\"").split('\n')))
+        return list(map( lambda x: x.split(','),fd.read().lower().replace('"','').split('\n')))
 
 def writeFile(name_file,table):
     with open(name_file,'w') as fd:
@@ -28,24 +43,37 @@ if (len(sys.argv) > 1):
     cidade = sys.argv[1] + '/'
 else:
     cidade = 'Guimaraes/'
+    #cidade = 'Porto/'
 
 #print(testa_dataSet())
 lista = readFile(cidade + 'ti.csv')[1:-1]
+#lista = readFile(cidade + 'Traffic_Incidents_Porto_Until_20190228.csv')[1:-1]
 #print(lista[3][3])
+
+def trata_rua(rua):
+    # retira os ()
+    # tira o espaco antes
+    #return unidecode.unidecode(re.sub(' \(.*?\)', '', rua).replace(" ",""))
+    return unidecode.unidecode(re.sub('\(.*', '',re.sub('\(.*\)', '', re.sub('\[.*?\]','',rua.replace(" ","")) )) )
+    
 
 
 #    3      |    4    |        5       |            7            |         8        |        9         |      10      
 # from_road | to_road | affected_roads | magnitude_of_delay_desc | length_in_meters | delay_in_seconds | incident_date
 # string    | string  |  | magnitude_of_delay_desc | length_in_meters | delay_in_seconds | incident_date
 
-from_road = list(map(lambda x: x[3],lista))
+from_road = list(map(lambda x: trata_rua(x[3]),lista))
+#print(len(from_road))
+
 #print(from_road)
 
-to_road = list(map(lambda x: x[4],lista))
+to_road = list(map(lambda x: trata_rua(x[4]),lista))
+#print(len(to_road))
 #print(to_road)
 #print(set(to_road))
 
-affected_roads = list(map(lambda x: x[5:-5] if x[5:-5]!= [''] else [],lista))
+affected_roads = list(map(lambda x: list(map(lambda y: trata_rua(y),x[5:-5])) if x[5:-5]!= [''] else [],lista))
+#print(len(affected_roads))
 #print(affected_roads)
 
 def rename_mag(string):
@@ -59,25 +87,61 @@ def rename_mag(string):
 # -4 7
 #magnitude_of_delay_desc = list(map(lambda x: rename_mag(x[-4]),lista))
 magnitude_of_delay_desc = list(map(lambda x: x[-4],lista))
+#print(len(magnitude_of_delay_desc))
 #print(magnitude_of_delay_desc)
 #print(set(magnitude_of_delay_desc))
 
 length_in_meters = list(map(lambda x: int(x[-3]),lista))
+#print(len(length_in_meters))
 #print(length_in_meters)
 #print(sorted(list(set(length_in_meters))))
 
 delay_in_seconds = list(map(lambda x: int(x[-2]),lista))
+#print(len(delay_in_seconds))
 #print(delay_in_seconds)
 
 incident_date = list(map(lambda x: x[-1],lista))
-#print(incident_date)
+#print(len(incident_date))
+#print()
+#print(len(lista))
+#print('\n'.join(incident_date))
+
+def separa_rua(rua,i):
+    res = ''
+    if(len(rua[i].split('/')) > 1):
+        res = rua[i].split('/')[1]
+        rua[i] = rua[i].split('/')[0]
+    return res
+
+
+for i in range(len(lista)):
+    from_road_aux = separa_rua(from_road,i)
+    to_road_aux = separa_rua(to_road,i)
+    if(from_road != '' or to_road != ''):
+        from_road.append(from_road_aux) if from_road_aux != '' else from_road.append(from_road[i]) 
+        to_road.append(to_road_aux) if to_road_aux != '' else to_road.append(to_road[i]) 
+
+        affected_roads.append(affected_roads[i])
+        magnitude_of_delay_desc.append(magnitude_of_delay_desc[i])
+        length_in_meters.append(length_in_meters[i])
+        delay_in_seconds.append(delay_in_seconds[i])
+        incident_date.append(incident_date[i]) 
+
+#print(len(from_road))
+#print(len(to_road))
+#print(len(affected_roads))
+#print(len(magnitude_of_delay_desc))
+#print(len(length_in_meters))
+#print(len(delay_in_seconds))
+#print(len(incident_date))
+
 
 def lltl(ll):
     return f.reduce(lambda x,y: x + y , ll)
 # todas as ruas
 #def ruas(): return list(set( from_road + to_road + affected_roads ))
 def ruas(): return filter(lambda x: x!='',list(set( from_road + to_road + lltl(affected_roads) )))
-             
+
 def create_graph():
     graph = {}
     for r in ruas():
@@ -88,7 +152,9 @@ def create_graph():
 def generate_graph():
     graph = create_graph()
     for x in range(1,len(lista)):
-        if (to_road[x] != '') : graph[ from_road[x] ].add(to_road[x])
+        if (to_road[x] != '') : 
+            graph[ from_road[x] ].add(to_road[x])
+            graph[ to_road[x] ].add(from_road[x])
         graph[ from_road[x] ].update(affected_roads[x])
     return graph
 
@@ -100,16 +166,69 @@ def generate_graph():
 
 def date(): return list(set(incident_date))
 
-def acidentes_date(date):
-    #return list(filter(lambda x: x[10] == hora,lista))
+def acidentes_date_func(date,f):
     res = []
-    for i,l in enumerate(lista):
-        if( x[10] == date ): res.append(i)
+    for i,l in enumerate(incident_date):
+        if( f(l) ): res.append(i)
+    return res
 
+def acidentes_date(date):
+    return acidentes_date_func(date,lambda x: x == date)
+
+def try_int(x):
+    try:
+        return int(x)
+    except:
+        return int(round(float(x)))
+
+def date_to_tuple(date):
+    try:
+        year , month, day = date[0].split('-')
+        hour, minute, second = date[1].split(':')
+    except:
+        date = date.split(' ')
+        year , month, day = date[0].split('-')
+        hour, minute, second = date[1].split(':')
+    #year , month, day = date[0].split('-')
+    #hour, minute, second = date[1].split(':')
+    return tuple(map(try_int,[year,month,day,hour,minute,second]))
+
+
+def date_to_datetime(date):
+    return datetime.strptime(date,'%Y-%m-%d %H:%M:%S.%f')
+
+#'2018-07-24 14:58:54.118000'
+def menor_date(date1, date2):
+    return date_to_tuple(date1) <= date_to_tuple(date2)
+
+def acidentes_menor_date(date):
+    return acidentes_date_func(date,lambda x: menor_date(date.split(' '),x.split(' ')))
 
 def fator_prop(dist):
    return dist
 
+def ordena(l):
+    return sorted([[ date_to_datetime(incident_date[i]),
+           from_road[i],
+           to_road[i],
+           affected_roads[i],
+           magnitude_of_delay_desc[i]] for i in l],key= lambda x: x[0])
+
+
+def new_date(date,range_date =12):
+    #hour, minute, second = list(map(try_int,date[1].split(':')))
+    #year, month, day = list(map(int,date[0].split('-')))
+    #return (datetime(year,month,day,hour,minute,second) - timedelta(hours = range_date) ).isoformat(' ').split(' ')
+    return date_to_datetime(date) - timedelta(hours = range_date) 
+            
+
+def filter_date(l,date,range_date):
+    res = []
+    for x in l:
+        if (x[0] > new_date(date,range_date)):
+            res.append(x)
+    return res
+    #return filter(lambda x: x[0] > new_date(date,range_date),l)
 
 #(magnitude_of_delay_desc[i], length_in_meters[i], delay_in_seconds[i]))
 def calc_peso_foco(peso):
@@ -120,7 +239,7 @@ def update_peso(peso):
     
 
 def forEach(f,l):
-    for x in f: f(x)
+    for x in l: f(x)
 
 def find_replace(foco,data,peso,table):
     # !!!!!!!!!!!!!!
@@ -164,6 +283,39 @@ def graph_date(date,graph):
                         delay_in_seconds[i]))
 
     
+
+def show_graph():
+    g = generate_graph()
+    ng = nx.DiGraph(g)
+    nx.draw(ng)
+    plt.show()
+
+def print_conect():
+    g = generate_graph()
+    res = set()
+    for x in g.keys():
+        #print(str(x) + ': ' + str(list(len(g[x]))))
+        print(x + '---> ' + str( len(list(g[x])) ))
+        #res.add()
+
+#print('numero de ruas: ' + str(len(list(ruas()))))
+#show_graph()
+#print_conect()
+#print(list(map(lambda x: ' '.join(x),ordena(acidentes_menor_date('2018-07-24 14:58:54.118000')))))
+#forEach(print,ordena(acidentes_menor_date('2018-07-24 14:58:54.118000')))
+#forEach(print,filter_date(ordena(range(len(incident_date))),'2019-02-28 19:45:01.855000',12))
+#print(new_date('2018-07-24 14:58:54.118000'.split(' '),99907))
+#print(max(acidentes_menor_date('2018-07-24 14:58:54.118000')))
+#print(len(lista))
+
+DataSet = filter_date(ordena(range(len(incident_date))),'2019-02-28 19:45:01.855000',12)
+#Output = readFile(OUTPUT)
+#if(len(Output[1]) == NUM_PARAMETROS_SEM_RUAS):
+    # criar uma 
+
+#for x in DataSet:
+
+
 
 
 
