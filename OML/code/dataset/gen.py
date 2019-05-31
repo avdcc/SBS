2b4251a8ac8c63e0d92e5eb9140ed8f083baacbc
ibanezg <ibanezg@ibanezg-System-Product-Name.home>
2019-05-31 13:23:38 +0100

    adicionar a margem e quadrantes 3D

--- OML/code/dataset/gen.py
+++ OML/code/dataset/gen.py
@@ -1,17 +1,34 @@
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import random
 import numpy as np
 
 # --------------------------------------------
 
+"""
+## Mostrar o grafico ##
+    INPUT:
+       * "lista" -- lista de n_uplos , sendo que as primeiras
+                    componentes correspondem as cordenadas
+                    e a ultima ao label / cor
+         
+    RETURN:
+       * "Pontos" -- escreve no std output os pontos e a sua classe
+"""
 def save(lista):
 
   print(len(lista), len(lista[0]) ,len(set(map(lambda x: x[-1], lista))))
   for i in lista:
     print (" ".join(map(str,i)))
 
-
+"""
+## Mostrar o grafico ##
+    INPUT:
+       * "lista" -- lista de n_uplos , sendo que as primeiras
+                    componentes correspondem as cordenadas
+                    e a ultima ao label / cor
+"""
 def pprint(lista):
 
   x = list(map(lambda x: x[0], lista))
@@ -21,21 +38,54 @@ def pprint(lista):
     c = list(map(lambda x: x[2], lista))
     plt.scatter(x,y,c=c)
   else:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
     z = list(map(lambda x: x[2], lista))
     c = list(map(lambda x: x[3], lista))
-    plt.scatter(x,y,z,c=c)
+    ax.scatter(x, y, z, c=c)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
 
   plt.show()
 
+
 # --------------------------------------------
 # LINHA
+"""
+## falcular o ponto na funcao##
+    INPUT:
+       * "x" -- valor de x
 
-def avalLinha(x,y,lista):
-
+       * "eq" -- equacao da funcao
+                onde e representada como uma lista de coeficientes
+         
+    RETURN:
+       * "y" -- valor da funcao
+"""
+def f(x,eq):
   res = 0
-  for i in range(len(lista[:-1])):
-    res += x**(len(lista)-i -1) * lista[i]
-  res += lista[-1]
+  for i in range(len(eq[:-1])):
+    res += x**(len(eq)-i -1) * eq[i]
+  res += eq[-1]
+  return res
+
+"""
+## Avalia o ponto em funcao da linha ##
+    INPUT:
+       * "x" -- valor de x
+       * "y" -- f(x)
+
+       * "eq" -- [f] equacao da funcao
+                onde e representada como uma lista de coeficientes
+         
+    RETURN:
+       * "res" -- tag
+"""
+def avalLinha(x,y,eq):
+  res = f(x,eq)
 
   if (res <= y):
     return 1
@@ -43,21 +93,40 @@ def avalLinha(x,y,lista):
     return 0
 
 
-def gen(n, lista, gen):
+"""
+## gera os pontos ##
+    INPUT:
+       * "n" -- numero de pontos
+       * "eq" -- [f] equacao da funcao
+                onde e representada como uma lista de coeficientes
+       * "g" -- dimensao
+       * "m" -- margem
+         
+    RETURN:
+       * "res" -- conjunto de pontos
+"""
+def gen(n, eq, g,m=0):
   # y = mx + b
   l = []
   for i in range(n):
 
-    x = random.uniform(-1,1)
-    y = random.uniform(-1,1)
+    while(True):
+        x = random.uniform(-1,1)
+        y = random.uniform(-1,1)
+        if(g==2):
+            if( abs(y - f(x,eq)) > m ):
+                break
+        elif(g==3):
+            z = random.uniform(-1,1)
+            if( (abs(x) > m) and (abs(y) > m) and (abs(z) > m) ):
+                break
 
-    if (gen == 3):
-      z = random.uniform(-1,1)
-      c = avalQuadrantes3(x,y,z,lista)
+    if (g == 3):
+      c = avalQuadrantes3(x,y,z,eq)
       l.append((x,y,z,c))
     else:
-      c = avalLinha(x,y,lista)
-      # c = avalQuadrantes2(x,y,lista)
+      c = avalLinha(x,y,eq)
+      # c = avalQuadrantes2(x,y,eq)
       l.append((x,y,c))
 
   return (l)
@@ -65,6 +134,18 @@ def gen(n, lista, gen):
 # --------------------------------------------
 # QUADRANTES
 
+"""
+## Avalia o ponto em funcao do Quadrante ##
+    INPUT:
+       * "x" -- valor de x
+       * "y" -- f(x)
+
+       * "lista" -- lista com as classes
+                    ou seja lista[i] = classe dos valores no quadrante i+1
+         
+    RETURN:
+       * "res" -- tag
+"""
 def avalQuadrantes2(x,y,lista):
   # len(lista) = 4
   if(x > 0 and y > 0):
@@ -78,6 +159,19 @@ def avalQuadrantes2(x,y,lista):
   else:
     print("...")
 
+"""
+## Avalia o ponto em funcao do Quadrante ##
+    INPUT:
+       * "x" -- valor de x
+       * "y" -- valor de y
+       * "z" -- valor de z
+
+       * "lista" -- lista com as classes
+                    ou seja lista[i] = classe dos valores no quadrante i+1
+         
+    RETURN:
+       * "res" -- tag
+"""
 def avalQuadrantes3(x,y,z,lista):
   # len(lista) = 8
   if(x > 0 and y > 0 and z > 0):
@@ -119,5 +213,7 @@ def avalQuadrantes3(x,y,z,lista):
 # --------------------------------------------
 
 plt.xkcd()
-save(gen(1500, [1,2,1,0], 2))
-# pprint3(gen(1500, [0,1,2,3,4,5,6,7]),3)
+#save(gen(1500, [1,2,1,0], 2))
+#pprint(gen(1500, [1,2,1,0],2,0.4))
+pprint(gen(19500, [1,2,3,4,5,6,7,8],3,0.3))
+#print(gen(1500, [1,2,3,4,5,6,7,8],3,0))
